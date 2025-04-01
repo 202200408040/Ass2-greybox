afe9b0bc95293815f5361bb8cc2aee5f2a84ffb0
202200408040 <2029911592@qq.com>
2025-04-01 18:35:34 +0800

    change

---
+++ greybox/Assets/NewBehaviourScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadAudioScene()
+    {
+        SceneManager.LoadScene("gery box1");
+    }
+}
